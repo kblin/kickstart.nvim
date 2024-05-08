cdf0a0866cd05846d4c219404a43f5362f76fcd3
Kai Blin <kblin@biosustain.dtu.dk>
2024-05-08 23:49:27 +0200

    init: set up tabstop/shiftwidth
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- init.lua
+++ init.lua
@@ -154,6 +154,10 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Set tabstop and shiftwidth to 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
